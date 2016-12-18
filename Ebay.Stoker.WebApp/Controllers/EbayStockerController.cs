521dbe34befd325aa2ab6efb6f4cd5ff721abfd1
avivsho <avivsho@gmail.com>
2016-12-18 23:56:21 +0200

    arrenging project

--- Ebay.Stoker.WebApp/Controllers/EbayStockerController.cs
+++ Ebay.Stoker.WebApp/Controllers/EbayStockerController.cs
@@ -3,15 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
+using eBay.Service.Call;
+using eBay.Service.Core.Sdk;
+using eBay.Service.Core.Soap;
+using Ebay.Stoker.WebApp.Models;
 
 namespace Ebay.Stoker.WebApp.Controllers
 {
     public class EbayStockerController : Controller
     {
+        private static ApiContext apiContext = null;
         // GET: EbayStocker
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Index(SellerSerchModel  serch) 
         {
-            return View();
+            //ApiContext apiContext = GetApiContext();
+            //GetSellerListCall apicall = new GetSellerListCall(apiContext);
+            //apicall.DetailLevelList.Add(DetailLevelCodeType.ReturnAll);
+            ////Pagination is required
+            //apicall.Pagination = new PaginationType();
+            //apicall.Pagination.PageNumber = 1;
+            //apicall.Pagination.EntriesPerPage = 200;
+            //apicall.UserID = serch.sellerName;
+            return View(serch);
+        }
+        /// <summary>
+        /// Populate eBay SDK ApiContext object with data from application configuration file
+        /// </summary>
+        /// <returns>ApiContext</returns>
+        static ApiContext GetApiContext()
+        {
+            //apiContext is a singleton,
+            //to avoid duplicate configuration reading
+            if (apiContext != null)
+            {
+                return apiContext;
+            }
+            else
+            {
+                apiContext = new ApiContext();
+
+                //set Api Server Url
+                apiContext.SoapApiServerUrl =
+                    ConfigurationManager.AppSettings["Environment.ApiServerUrl"];
+                //set Api Token to access eBay Api Server
+                ApiCredential apiCredential = new ApiCredential();
+                apiCredential.eBayToken =
+                    ConfigurationManager.AppSettings["UserAccount.ApiToken"];
+                apiContext.ApiCredential = apiCredential;
+                //set eBay Site target to US
+                apiContext.Site = SiteCodeType.US;
+
+                return apiContext;
+            }
         }
     }
 }
--- Ebay.Stoker.WebApp/Controllers/HomeController.cs
+++ Ebay.Stoker.WebApp/Controllers/HomeController.cs
@@ -12,19 +12,9 @@ namespace Ebay.Stoker.WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private static ApiContext apiContext = null;
-        public ActionResult Index(string period, string sellerid)
+       
+        public ActionResult Index()
         {
-
-            //ApiContext apiContext = GetApiContext();
-            //GetSellerListCall apicall = new GetSellerListCall(apiContext);
-            //apicall.DetailLevelList.Add(DetailLevelCodeType.ReturnAll);
-            //Pagination is required
-            //apicall.Pagination = new PaginationType();
-            //apicall.Pagination.PageNumber = 1;
-            //apicall.Pagination.EntriesPerPage = 200;
-            //if (campatitor.Length > 0)
-            //    apicall.UserID = campatitor;
             return View();
         }
 
@@ -45,36 +35,7 @@ namespace Ebay.Stoker.WebApp.Controllers
         {
             return View();
         }
-        /// <summary>
-        /// Populate eBay SDK ApiContext object with data from application configuration file
-        /// </summary>
-        /// <returns>ApiContext</returns>
-        static ApiContext GetApiContext()
-        {
-            //apiContext is a singleton,
-            //to avoid duplicate configuration reading
-            if (apiContext != null)
-            {
-                return apiContext;
-            }
-            else
-            {
-                apiContext = new ApiContext();
 
-                //set Api Server Url
-                apiContext.SoapApiServerUrl =
-                    ConfigurationManager.AppSettings["Environment.ApiServerUrl"];
-                //set Api Token to access eBay Api Server
-                ApiCredential apiCredential = new ApiCredential();
-                apiCredential.eBayToken =
-                    ConfigurationManager.AppSettings["UserAccount.ApiToken"];
-                apiContext.ApiCredential = apiCredential;
-                //set eBay Site target to US
-                apiContext.Site = SiteCodeType.US;
-
-                return apiContext;
-            }
-        }
     }
 
 }
